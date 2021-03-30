5e5e82b218ed5306befac0ca9b5826ef8b075563
mae-yosores2k19 <jessamaehortadoyosores@gmail.com>
2021-03-30 21:19:03 +0800

    update

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -22,8 +22,9 @@ export class AuthService {
   public user: Observable<any>;
   // AUTH_SERVER_ADDRESS: string = 'http://msqcustomerinterfacebackend-env-1.eba-negj35aw.us-east-2.elasticbeanstalk.com/';
   // AUTH_SERVER_ADDRESS: string = 'http://localhost:8080/';
-  AUTH_SERVER_ADDRESS: string = 'http://3.137.219.17:8080/';
-
+  // AUTH_SERVER_ADDRESS: string = 'http://3.137.219.17:8080/';
+  AUTH_SERVER_ADDRESS: string = 'https://msqcustomerbackend.herokuapp.com/';
+  
   authSubject = new BehaviorSubject(false);
 
 
